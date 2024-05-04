b8b7b7020fd33b11326233af277f7d0e1ad5b638
Vera Wells <palaven@Palaven.lan>
2024-05-04 07:09:14 -0400

    file structure and file import

--- src/fossey_reader/AncientDocumentReader.py
+++ src/fossey_reader/AncientDocumentReader.py
@@ -1,5 +1,19 @@
 
+import cv2
+import os
+
+
 class AncientDocumentReader:
 
-    def __init__ (self, fn):
-        self.filename = fn
+    def __init__ (self, document_directory):
+        self.doc_dir = document_directory
+
+    
+    def load_images_from_folder(self):
+        images = []
+        for filename in os.listdir(self.doc_dir):
+            print(filename)
+            img = cv2.imread(os.path.join(self.doc_dir, filename))
+            if img is not None:
+                images.append(img)
+        return images
--- src/fossey_reader/main_fossey_ocr.py
+++ src/fossey_reader/main_fossey_ocr.py
@@ -1,27 +1,13 @@
 
-import json
-import cv2
-import os
-
-import AncientDocumentReader
+from AncientDocumentReader import AncientDocumentReader
 
 
-def load_images_from_folder(folder):
-    images = []
-    for filename in os.listdir(folder):
-        print(filename)
-        img = cv2.imread(os.path.join(folder,filename))
-        if img is not None:
-            images.append(img)
-    return images
 
 
 if __name__ == '__main__':
-    # Let's import some images, test sample first
-    print("We're doing stuff here now")
-
 
     # TODO: pass the dataset folder as an argument or something?
     folder = "dataset/"
+    myAncientReader = AncientDocumentReader(folder)
 
-    myImages = load_images_from_folder(folder)
+    myImages = myAncientReader.load_images_from_folder()
